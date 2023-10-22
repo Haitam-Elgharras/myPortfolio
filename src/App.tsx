a0a1da13648943498b524dd02903018f4747b0be
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-10-22 23:31:35 +0100

    Starting Qualification section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "./components/about/AboutSection";
 import HomeContent from "./components/home/HomeContent";
 import Navbar from "./components/Navbar/Navbar";
+import QualificationSection from "./components/qualification/QualificationSection";
 import Skills from "./components/Skills/SkillsSection";
 import "./style.css";
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <HomeContent />
         <AboutSection />
         <Skills />
+        <QualificationSection />
       </main>
     </>
   );
---
+++ src/components/qualification/QualificationSection.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../SectionTitle";
+
+const QualificationSection = () => {
+  return (
+    <section className="qualification section">
+      <SectionTitle title="Qualification" subtitle="My personal journey" />
+    </section>
+  );
+};
+
+export default QualificationSection;
