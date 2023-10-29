4da74958436c83698dc0592c8cba8ddb62a019af
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-10-29 21:48:13 +0100

    add PortfolioItem component

--- src/components/portfolio/PortfolioSection.tsx
+++ src/components/portfolio/PortfolioSection.tsx
@@ -1,9 +1,18 @@
 import SectionTitle from "../SectionTitle";
+import PortfolioItem from "./ProtfolioItem";
+import ptfl1 from "../../assets/img/portfolio1.jpg";
 
 const PortfolioSection = () => {
   return (
     <section className="portfolio section" id="portfolio">
       <SectionTitle title="Portfolio" subtitle="Most recent work" />
+      {/* test portfolio item */}
+      <PortfolioItem
+        imageSrc={ptfl1}
+        title="Portfolio Test"
+        description="This portfolio website was created using React and TypeScript."
+        demoLink="#"
+      />
     </section>
   );
 };
---
+++ src/components/portfolio/ProtfolioItem.tsx
@@ -0,0 +1,36 @@
+interface Props {
+  imageSrc: string;
+  title: string;
+  description: string;
+  demoLink: string;
+}
+
+const PortfolioItem = ({ imageSrc, title, description, demoLink }: Props) => {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr",
+        gap: "2rem",
+        padding: "2rem",
+        justifyContent: "center",
+      }}
+      className="portfolio__content grid swiper-slide"
+    >
+      <img src={imageSrc} alt="" className="portfolio__img" />
+      <div className="portfolio__data">
+        <h3 className="portfolio__title">{title}</h3>
+        <p className="portfolio__description">{description}</p>
+        <a
+          href={demoLink}
+          className="button button--flex button--small portfolio__button"
+        >
+          Demo
+          <i className="uil uil-arrow-right button__icon"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioItem;
