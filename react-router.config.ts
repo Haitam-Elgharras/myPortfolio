f682e29d218b6c3c392d1aab9d6e5e4ed5d51f2a
haitam-elgharras <elgharras.haitam@gmail.com>
2026-09-12 14:51:03 +0100

    chore: enable react router v8 future flags

--- react-router.config.ts
+++ react-router.config.ts
@@ -19,6 +19,14 @@ export default {
 
   presets: [vercelPreset()],
 
+  future: {
+    v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+    v8_passThroughRequests: true,
+    v8_trailingSlashAwareDataRequests: true,
+  },
+
   prerender: {
     paths: async ({ getStaticPaths }) => [
       ...getStaticPaths(),
